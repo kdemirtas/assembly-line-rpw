e829c0d73edd1bb271b5ec0823dc0446030647be
kdemirta <keremdemirtas@gmail.com>
2022-01-04 22:18:27 +0300

    solver implemented

--- assembly_line_rpw/model.py
+++ assembly_line_rpw/model.py
@@ -1,11 +1,13 @@
 class Task:
     def __init__(self, id, duration, pred_list):
         self.id = id
+        self.station_id = None
         self.duration = duration
         self.pred_list = pred_list
         self.succ_list = []
         self.all_succ_list = []
         self.rpw = None
+        self.assigned = False
         print(self.id, self.duration, self.pred_list)
 
     def add_successor(self, succ):
@@ -25,21 +27,32 @@ class Task:
                 if not succ in self.all_succ_list:
                     self.all_succ_list.append(succ)
                     self.find_all_successors(task_dict, task_dict[succ])
+
+    def assign_to_station(self, station_id):
+        self.station_id = station_id
+        self.assigned = True
+
+    def is_assigned(self):
+        return self.assigned
                 
                 
 class Station:
-    def __init__(self):
+    def __init__(self, station_id):
+        self.station_id = station_id
         self.tasks = []
+        self.tasks_list = []
+        self.station_time = 0
 
     def assigned_tasks(self):
         return self.tasks
 
     def add_task(self, task):
         self.tasks.append(task)
+        self.tasks_list.append(task.id)
+        self.station_time += task.duration
 
 class Model:
     def __init__(self, settings, tasks, task_dict):
         self.tasks = tasks
         self.task_dict = task_dict
         self.settings = settings
-        self.stations = []
--- assembly_line_rpw/solver.py
+++ assembly_line_rpw/solver.py
@@ -1,16 +1,23 @@
 from collections import OrderedDict
+from assembly_line_rpw.model import Station
 
 
 class Solution:
     def __init__(self):
-        pass
+        self.stations = []
+        self.n_stations = 0
+
+    def add_station(self, station):
+        self.stations.append(station)
+
 
 class Solver:
     def __init__(self, model):
         self.model = model
-        self.solution = {}
+        self.solution = Solution()
 
     def solve(self):
+        CYCLE_TIME = self.model.settings["CYCLE_TIME"]
         # Find all the successors and compute the rpw values for every task
         for task in self.model.task_dict.values():
             task.find_all_successors(self.model.task_dict)
@@ -18,7 +25,42 @@ class Solver:
 
         # Sort tasks by rpw values in descending order
         task_dict_sorted = OrderedDict(sorted(self.model.task_dict.items(), key=lambda x: x[1].rpw, reverse=True))
+        task_dict_assigned = {task_id: task.is_assigned() for task_id, task in task_dict_sorted.items()}
+
+        # Assign tasks to stations
+        s = 1
+
+        # Create first station if there are no stations
+        if not self.solution.stations:
+            station = Station(s)
+            self.solution.add_station(station)
+            s = s + 1
+
+        while not all(list((task_dict_assigned.values()))):
+            for task_id, task in task_dict_sorted.items():
+                if not task.is_assigned():
+                    task_duration = task.duration
+                    if (station.station_time + task_duration <= CYCLE_TIME) and self._check_preds(task, task_dict_sorted):
+                        station.add_task(task)
+                        task.assign_to_station(station.station_id)
+                        task_dict_assigned[task_id] = True                       
+                    else:
+                        if station.station_time < CYCLE_TIME:
+                            continue
+            if not all(list((task_dict_assigned.values()))):
+                station = Station(s)
+                self.solution.add_station(station)
+                s = s + 1
+            
+
+    def _check_preds(self, task, task_dict_sorted):
+        check_list = []
+        for pred in task.pred_list:
+            check_list.append(task_dict_sorted[pred].is_assigned())
+        return all(check_list)
+
         return self.solution
+
 
 
     def print_solution(self):
