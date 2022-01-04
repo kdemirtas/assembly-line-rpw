8c3edf3f546afc8f709547e6824b9db6f8f9ee25
kdemirta <keremdemirtas@gmail.com>
2022-01-04 11:02:58 +0300

    initial commit

---
+++ assembly_line_rpw/solve.py
@@ -0,0 +1,8 @@
+### This will be the main solver ###
+
+class Solver:
+    def __init__(self):
+        pass
+
+    def solve():
+        return None
---
+++ assembly_line_rpw/task.py
@@ -0,0 +1,5 @@
+class Task:
+    def __init__(self, id, duration, pred):
+        self.id = id
+        self.duration = duration
+        self.pred = pred
